e5d34775f9e0970d020bf6e996880f1c5e2a7a7f
Tom Österlund <tom.osterlund1@gmail.com>
2024-09-04 08:45:34 +0200

    add events service plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <div>
 	<ScheduleXCalendar calendarApp="{calendarApp}" timeGridEvent="{TimeGridEvent}" />
+
+	<button on:click={addEvent}>add event</button>
 </div>
 
 <script lang="ts">
@@ -7,21 +9,35 @@
 	import { createCalendar, viewDay, viewWeek } from '@schedule-x/calendar';
 	import '@schedule-x/theme-default/dist/index.css';
 	import TimeGridEvent from '$lib/TimeGridEvent.svelte';
+	import { createEventsServicePlugin } from '@schedule-x/events-service';
+
+	let eventsService = createEventsServicePlugin()
+
+	const addEvent = () => {
+		eventsService.add({
+			id: '3',
+			title: 'Event 3',
+			start: '2024-09-06 06:00',
+			end: '2024-09-06 08:00',
+		})
+	};
 
 	const calendarApp = createCalendar({
 		views: [viewDay, viewWeek],
+		plugins: [eventsService],
+		selectedDate: '2024-09-04',
 		events: [
 			{
 				id: '1',
 				title: 'Event 1',
-				start: '2024-07-06',
-				end: '2024-07-06',
+				start: '2024-09-06',
+				end: '2024-09-06',
 			},
 			{
 				id: '2',
 				title: 'Event 2',
-				start: '2024-07-06 02:00',
-				end: '2024-07-06 04:00',
+				start: '2024-09-06 02:00',
+				end: '2024-09-06 04:00',
 			},
 		]
 	})
